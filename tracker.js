aa16596e2ff65be42a42a761b087f18902792b96
harryfoto <dr.hwan@gmail.com>
2026-05-30 20:56:20 +0900

    Update tracker.js

--- tracker.js
+++ tracker.js
@@ -20,12 +20,39 @@ const { chromium } = require('playwright');
   const html = await page.content();
 
   const match =
-    html.match(
-      /"follower_count":(\d+)/
+    html.match(/"follower_count":(\d+)/);
+
+  if (!match) {
+    throw new Error(
+      'Follower count not found'
+    );
+  }
+
+  const followers =
+    Number(match[1]);
+
+  console.log(
+    'Followers:',
+    followers
+  );
+
+  const response =
+    await fetch(
+      'https://script.google.com/macros/s/AKfycbxUNGD5Ko1kSIdEhAv_Utuhth3gUGSKW7wLH0RKEzLtFOGla_dpBGLcGQvCcUoYpqzLsg/exec',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type':
+            'application/json'
+        },
+        body: JSON.stringify({
+          followers
+        })
+      }
     );
 
   console.log(
-    match ? match[1] : 'NOT_FOUND'
+    await response.text()
   );
 
   await browser.close();
