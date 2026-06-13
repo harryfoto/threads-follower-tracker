9072352cabbb30b16ff03be4465ee3502b8a5e63
Harry <dr.hwan@gmail.com>
2026-06-13 21:10:54 +0900

    Update tracker.js

--- tracker.js
+++ tracker.js
@@ -38,7 +38,7 @@ const { chromium } = require('playwright');
 
   const response =
     await fetch(
-      'https://script.google.com/macros/s/AKfycbxUNGD5Ko1kSIdEhAv_Utuhth3gUGSKW7wLH0RKEzLtFOGla_dpBGLcGQvCcUoYpqzLsg/exec',
+      'https://script.google.com/macros/s/AKfycbxszjhcDEekMcZo_iiof0elbeouXA38g7L-oejw4lOtJy3XLkmdNp42uX6E7RBciWWxpA/exec',      
       {
         method: 'POST',
         headers: {
